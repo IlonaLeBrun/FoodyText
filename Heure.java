11502b4860ec62fd1b4b4604f3c94eef215c60a7
Le Brun <ilona.lebrun.sio@gmail.com>
2017-09-06 16:41:20 +0200

    setter

--- Heure.java
+++ Heure.java
@@ -13,4 +13,14 @@ public class Heure{
 	public int getMinutes(){
 		
 	}
+	
+	public int setHeures(){
+		this.heures = heures;
+	}
+
+        public int setMinutes(){
+                this.minutes = minutes;
+        }
+
+
 }
